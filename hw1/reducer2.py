8c5462a8fa4879b3de5a6ca2108af5be8da07ef2
Shaktidhar <shaktidhark.1997@gmail.com>
2024-10-04 08:54:58 -0400

    hw1

---
+++ hw1/reducer2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#Shaktidhar Kanamarla sk10945
+# HW1 - MapReduce - Reducer
+
+import sys
+from collections import defaultdict
+from operator import itemgetter
+
+def read_mapper_output(file):
+    for line in file:
+        yield line.strip().split('\t', 1)
+
+def main():
+    word_counts = defaultdict(int)
+
+    # Read the data from standard input
+    for word, count in read_mapper_output(sys.stdin):
+        word_counts[word] += int(count)
+
+    # Sort the word counts in descending order
+    sorted_word_counts = sorted(word_counts.items(), key=itemgetter(1), reverse=True)
+
+    # Output the top 10 words
+    for id, (word, count) in enumerate(sorted_word_counts, start=1):
+        print(f"{id},{word}")
+
+if __name__ == "__main__":
+    main()
